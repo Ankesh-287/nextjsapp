74cf1de9ef3a9f5255aca404f98fa25d09a5612f
Ankesh-287 <ankeshpatil287@gmail.com>
2024-12-26 12:37:49 +0530

    just commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -75,7 +75,7 @@ export default function SignupPage() {
             type="submit"
             onClick={onSingup}
             className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-            >{buttonDisabled ? "No signup" : "Signup" }</button>
+            >{buttonDisabled ? "Signup" : "Can't Signup" }</button>
             <Link href="/login"> Visit Login Page</Link>
         </div>
     )
